b43b2b5daded18cf2dc6ec9b6664110942d63c76
Povilas Kanapickas <povilas@radix.lt>
2025-06-20 07:15:36 +0300

    Enable SM 8.6

--- vllm_flash_attn/flash_attn_interface.py
+++ vllm_flash_attn/flash_attn_interface.py
@@ -42,11 +42,10 @@ def _is_fa3_supported(device = None) -> Tuple[bool, Optional[str]]:
         return False, f"FA3 is unavaible due to: {FA3_UNAVAILABLE_REASON}"
     if torch.cuda.get_device_capability(device)[0] < 8 \
         or torch.cuda.get_device_capability(device)[0] >= 10 \
-        or torch.cuda.get_device_capability(device) == (8, 6) \
         or torch.cuda.get_device_capability(device) == (8, 9):
         return False, \
             "FA3 is only supported on devices with compute capability >= 8" \
-            " excluding 8.6 and 8.9 and Blackwell archs (>=10)"
+            " excluding 8.9 and Blackwell archs (>=10)"
     return True, None
 
 def is_fa_version_supported(fa_version: int, device = None) -> bool:
